ad09f0d133c89f3089e1548162d802c2ef1b27aa
rudolfbsuguitan <rudolf.suguitan@gmail.com>
2017-02-23 13:11:36 +0000

    detecting sign

---
+++ signdetect.py
@@ -0,0 +1,31 @@
+import numpy as np
+import cv2
+import time
+
+
+video = cv2.VideoCapture(0)
+time.sleep(0.1)
+
+#cv2.namedWindow("Main Frame"", cv2.WINDOW_AUTOSIZE)
+sign_cascade = cv2.CascadeClassifier('/opencv-3.1.0/data/haarcascades/cascade.xml')
+
+
+while True:
+        ret, OriginalFrame = video.read()
+        gray = cv2.cvtColor(OriginalFrame, cv2.COLOR_BGR2GRAY)
+
+        signs = sign_cascade.detectMultiScale(gray, 5, 5)
+
+        for (x,y,w,h) in signs:
+                cv2.rectangle(OriginalFrame,(x,y),(x+w,y+h),(255,255,0),2)
+                font = cv2.FONT_HERSHEY_SIMPLEX
+                cv2.putText(OriginalFrame, 'Traffic', (x+w, y+h), font, 1, (0, 255, 255), 2, cv2.LINE_AA)
+        cv2.imshow("Main Frame", OriginalFrame)
+
+        k = cv2.waitKey(1) & 0xFF
+        if k == 27:
+                break
+
+video.release()
+cv2.destroyAllWindows()
+
