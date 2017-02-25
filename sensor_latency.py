5bf08d6304286d5372e2a58488390405fb145919
RudolfBSuguitan <rudolf.suguitan@gmail.com>
2017-02-25 16:47:30 +0000

    update

--- sensor_latency.py
+++ sensor_latency.py
@@ -1,29 +1,27 @@
 import pygame
 pygame.mixer.init()
 
-import RPi.GPIO as GPIO                    #Import GPIO library
-import time                                #Import time library
-GPIO.setmode(GPIO.BCM)                     #Set GPIO pin numbering 
+import RPi.GPIO as GPIO
+import time
+GPIO.setmode(GPIO.BCM)
 
 def usensor(trig, echo):
         GPIO.output(trig, False)
-        #print "Front"
-        #time.sleep(0.05)
 
         GPIO.output(trig, True)
-        time.sleep(0.00001)                      #Delay of 0.00001 seconds Provide trigger signal to TRIG input, it requires a HIGH signal of atleast 
-        GPIO.output(trig, False)
+        time.sleep(0.00001)
+	GPIO.output(trig, False)
 
-        while GPIO.input(echo)==0:               #Check whether the ECHO is LOW
-                pulse_start = time.time()              #Saves the last known time of LOW pulse
+        while GPIO.input(echo)==0:
+                pulse_start = time.time()
 
-        while GPIO.input(echo)==1:               #Check whether the ECHO is HIGH
-                pulse_end = time.time()                #Saves the last known time of HIGH pulse
+        while GPIO.input(echo)==1:
+                pulse_end = time.time()
 
-        pulse_duration = pulse_end - pulse_start #Get pulse duration to a variable
+        pulse_duration = pulse_end - pulse_start
 
-        distance = pulse_duration * 17150        #Multiply pulse duration by 17150 to get distance
-        distance = round(distance, 2)            #Round to two decimal points
+        distance = pulse_duration * 17150
+        distance = round(distance, 2)
 
         return distance
 
@@ -46,12 +44,7 @@ def curPos(warn_dist):
                 if dist_up >= distance and dist_down <= distance:
                         time.sleep(0.2)
                         distance = usensor(TRIGFRONT, ECHOFRONT)
-                        print "Stationary Decrease: ", distance
-                #elif dist_down >= distance:
-                #       while True:
-                #               print "Distance Increase: ", distance
-		#               if dist_up < distance:
-                #                       break 
+                        print "Stationary Decrease: ", distance 
                 else:
                         break
 
@@ -65,20 +58,16 @@ while True:
 
         distance = usensor(TRIGFRONT, ECHOFRONT)
 
-        if distance > 100:      #Check whether the distance is within range
-                print "Distance Front:",distance - 0.5,"cm"  #Print distance with 0.5 cm calibration
+        if distance > 100:
+                print "Distance Front:",distance - 0.5,"cm"
         if distance < 100:
-                print "Checking Distance:",distance - 0.5,"cm"  #Print distance with 0.5 cm calibration
+                print "Checking Distance:",distance - 0.5,"cm"
 		time.sleep(0.5)
                 distance = usensor(TRIGFRONT, ECHOFRONT)
                 if distance < 100:
-                        print "WARNING", distance                   #display out of range
-                        #warningFront()
+                        print "WARNING", distance
                         warn_dist = distance
 
                         curPos(warn_dist)
-                        #distance = usensor(TRIGFRONT, ECHOFRONT)
-
-                        #if 
 
 
