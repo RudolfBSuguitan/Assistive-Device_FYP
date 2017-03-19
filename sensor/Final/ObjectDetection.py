6b6e9b56aa38da378fb3d759950e8b1800c087e8
rudolfbsuguitan <rudolf.suguitan@gmail.com>
2017-03-19 22:49:38 +0000

    Threading

--- sensor/Final/ObjectDetection.py
+++ sensor/Final/ObjectDetection.py
@@ -1,89 +1,93 @@
 import numpy as np
 import cv2
 import time
+import threading
 
-
-video = cv2.VideoCapture(0)
-time.sleep(3)
-
-def itemPos(x):
-	if x <= 150:
-		print "Left Object"
-	elif x > 150 and x <= 395:
-		print "Centre Object"
-	elif x > 395:
-		print "Right Object"
-	return
-	
+LOOP=True
 
 #cv2.namedWindow("Main Frame"", cv2.WINDOW_AUTOSIZE)
 dBus = cv2.CascadeClassifier('/home/pi/Documents/Assistive-Device_FYP/cascades/dbus.xml')
-const = cv2.CascadeClassifier('/home/pi/Documents/Assistive-Device_FYP/cascades/Construction2.xml')
+#const = cv2.CascadeClassifier('/home/pi/Documents/Assistive-Device_FYP/cascades/Construction2.xml')
 cleanSign = cv2.CascadeClassifier('/home/pi/Documents/Assistive-Device_FYP/cascades/CleaningSign.xml')
-rLight = cv2.CascadeClassifier('/home/pi/Documents/Assistive-Device_FYP/cascades/rLight.xml')
-noPed = cv2.CascadeClassifier('/home/pi/Documents/Assistive-Device_FYP/cascades/noPed.xml')
+#rLight = cv2.CascadeClassifier('/home/pi/Documents/Assistive-Device_FYP/cascades/rLight.xml')
+#noPed = cv2.CascadeClassifier('/home/pi/Documents/Assistive-Device_FYP/cascades/noPed.xml')
 pedBtn = cv2.CascadeClassifier('/home/pi/Documents/Assistive-Device_FYP/cascades/pedButton.xml')
 TrafL = cv2.CascadeClassifier('/home/pi/Documents/Assistive-Device_FYP/cascades/TrafficL2.xml')
 stop2 = cv2.CascadeClassifier('/home/pi/Documents/Assistive-Device_FYP/cascades/Stop2.xml')
 
+def itemPos(x):
+        if x <= 150:
+                print "Left Object"
+        elif x > 150 and x <= 395:
+                print "Centre Object"
+        elif x > 395:
+                print "Right Object"
+        return
 
-while True:
-        ret, OriginalFrame = video.read()
-        gray = cv2.cvtColor(OriginalFrame, cv2.COLOR_BGR2GRAY)
+def camThread():
+	video = cv2.VideoCapture(0)
+	time.sleep(3)
+	global LOOP
 
-        #signs = sign_cascade.detectMultiScale(gray, 5, 5)
-        ped = pedBtn.detectMultiScale(gray, scaleFactor=1.2, minNeighbors=6, minSize=(40, 40), maxSize=(90,90))
-	#traffic_light2 traf = TrafL.detectMultiScale(gray, scaleFactor=1.1, minNeighbors=3, minSize=(45, 45), maxSize=(60, 60))
-	traf = TrafL.detectMultiScale(gray, scaleFactor=1.15, minNeighbors=3, minSize=(60, 60), maxSize=(80, 80))
-	cSign = cleanSign.detectMultiScale(gray, scaleFactor=1.15, minNeighbors=3, minSize=(65, 65), maxSize=(90,90))
-	stopSign = stop2.detectMultiScale(gray, scaleFactor=1.2, minNeighbors=6, minSize=(60, 60), maxSize=(80, 80))
-	Bus = dBus.detectMultiScale(gray, scaleFactor=1.2, minNeighbors=6, minSize=(55, 55), maxSize=(75, 75))
+	while LOOP:
+        	ret, OriginalFrame = video.read()
+        	gray = cv2.cvtColor(OriginalFrame, cv2.COLOR_BGR2GRAY)
 
-	print traf
-	for (x,y,w,h) in Bus:
-                cv2.rectangle(OriginalFrame,(x,y),(x+w,y+h),(255,255,0),2)
-                font = cv2.FONT_HERSHEY_SIMPLEX
-                cv2.putText(OriginalFrame, 'Bus Stop', (x+w, y+h), font, 1, (0, 255, 255), 2, cv2.LINE_AA)
-                print "Bus Stop Detected"
-                itemPos(x)
+        	#signs = sign_cascade.detectMultiScale(gray, 5, 5)
+        	ped = pedBtn.detectMultiScale(gray, scaleFactor=1.2, minNeighbors=6, minSize=(40, 40), maxSize=(90,90))
+		#traffic_light2 traf = TrafL.detectMultiScale(gray, scaleFactor=1.1, minNeighbors=3, minSize=(45, 45), maxSize=(60, 60))
+		traf = TrafL.detectMultiScale(gray, scaleFactor=1.15, minNeighbors=3, minSize=(60, 60), maxSize=(80, 80))
+		cSign = cleanSign.detectMultiScale(gray, scaleFactor=1.15, minNeighbors=3, minSize=(65, 65), maxSize=(90,90))
+		stopSign = stop2.detectMultiScale(gray, scaleFactor=1.2, minNeighbors=6, minSize=(60, 60), maxSize=(80, 80))
+		Bus = dBus.detectMultiScale(gray, scaleFactor=1.2, minNeighbors=6, minSize=(55, 55), maxSize=(75, 75))
 
-	for (x,y,w,h) in traf:
-                cv2.rectangle(OriginalFrame,(x,y),(x+w,y+h),(255,255,0),2)
-                font = cv2.FONT_HERSHEY_SIMPLEX
-                cv2.putText(OriginalFrame, 'TrafficLight', (x+w, y+h), font, 1, (0, 255, 255), 2, cv2.LINE_AA)
-                print "Traffic Light Detected"
-                itemPos(x)
+		print traf
+		for (x,y,w,h) in Bus:
+                	cv2.rectangle(OriginalFrame,(x,y),(x+w,y+h),(255,255,0),2)
+                	font = cv2.FONT_HERSHEY_SIMPLEX
+                	cv2.putText(OriginalFrame, 'Bus Stop', (x+w, y+h), font, 1, (0, 255, 255), 2, cv2.LINE_AA)
+                	print "Bus Stop Detected"
+                	itemPos(x)
 
-
-        for (x,y,w,h) in ped:
-                cv2.rectangle(OriginalFrame,(x,y),(x+w,y+h),(255,255,0),2)
-                font = cv2.FONT_HERSHEY_SIMPLEX
-                cv2.putText(OriginalFrame, 'Button', (x+w, y+h), font, 1, (0, 255, 255), 2, cv2.LINE_AA)
-		print "Button Detected"
-		itemPos(x)
-
-	for (x,y,w,h) in stopSign:
-                cv2.rectangle(OriginalFrame,(x,y),(x+w,y+h),(255,255,0),2)
-                font = cv2.FONT_HERSHEY_SIMPLEX
-                cv2.putText(OriginalFrame, 'Stop', (x+w, y+h), font, 1, (0, 255, 255), 2, cv2.LINE_AA)
-                print "Stop Sign Detected"
-                itemPos(x)
-
-	for (x,y,w,h) in cSign:
-                cv2.rectangle(OriginalFrame,(x,y),(x+w,y+h),(255,255,0),2)
-                font = cv2.FONT_HERSHEY_SIMPLEX
-                cv2.putText(OriginalFrame, 'Clean', (x+w, y+h), font, 1, (0, 255, 255), 2, cv2.LINE_AA)
-                print "Clean Sign Detected"
-                itemPos(x)
+		#for (x,y,w,h) in traf:
+                	#cv2.rectangle(OriginalFrame,(x,y),(x+w,y+h),(255,255,0),2)
+                	#font = cv2.FONT_HERSHEY_SIMPLEX
+                	#cv2.putText(OriginalFrame, 'TrafficLight', (x+w, y+h), font, 1, (0, 255, 255), 2, cv2.LINE_AA)
+                	#print "Traffic Light Detected"
+                	#itemPos(x)
 
 
+        	for (x,y,w,h) in ped:
+                	cv2.rectangle(OriginalFrame,(x,y),(x+w,y+h),(255,255,0),2)
+                	font = cv2.FONT_HERSHEY_SIMPLEX
+                	cv2.putText(OriginalFrame, 'Button', (x+w, y+h), font, 1, (0, 255, 255), 2, cv2.LINE_AA)
+			print "Button Detected"
+			itemPos(x)
 
-	cv2.imshow("Main Frame", OriginalFrame)
+		for (x,y,w,h) in stopSign:
+                	cv2.rectangle(OriginalFrame,(x,y),(x+w,y+h),(255,255,0),2)
+                	font = cv2.FONT_HERSHEY_SIMPLEX
+                	cv2.putText(OriginalFrame, 'Stop', (x+w, y+h), font, 1, (0, 255, 255), 2, cv2.LINE_AA)
+                	print "Stop Sign Detected"
+                	itemPos(x)
 
-        k = cv2.waitKey(1) & 0xFF
-        if k == 27:
-                break
+		for (x,y,w,h) in cSign:
+                	cv2.rectangle(OriginalFrame,(x,y),(x+w,y+h),(255,255,0),2)
+                	font = cv2.FONT_HERSHEY_SIMPLEX
+                	cv2.putText(OriginalFrame, 'Clean', (x+w, y+h), font, 1, (0, 255, 255), 2, cv2.LINE_AA)
+                	print "Clean Sign Detected"
+                	itemPos(x)
 
-video.release()
-cv2.destroyAllWindows()
 
+
+		cv2.imshow("Main Frame", OriginalFrame)
+
+        	k = cv2.waitKey(1) & 0xFF
+        	if k == 27:
+                	LOOP=False
+
+	video.release()
+	cv2.destroyAllWindows()
+
+camT = threading.Thread(target=camThread, args=())
+camT.start()
